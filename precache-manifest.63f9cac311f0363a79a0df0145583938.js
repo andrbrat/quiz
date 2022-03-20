7cd8b7af7c2a603ca384acdfb06016f694937295
Andrey Bratanov <prowebber.code@gmail.com>
2022-03-20 16:05:47 +0500

    adding dist subtree

---
+++ precache-manifest.63f9cac311f0363a79a0df0145583938.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ce6645d0e39c34ebcf",
+    "url": "/quiz/css/app.ca936927.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/quiz/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b097c2dbc7713a9da8d6ad3aed57880d",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "f4ce6645d0e39c34ebcf",
+    "url": "/quiz/js/app.105993d3.js"
+  },
+  {
+    "revision": "c9a8167afdb8361e76a2",
+    "url": "/quiz/js/chunk-vendors.b3c6feba.js"
+  },
+  {
+    "revision": "53472f14e238dd5b39f843a61bc9f234",
+    "url": "/quiz/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/quiz/robots.txt"
+  }
+]);
